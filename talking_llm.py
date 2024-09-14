bae17b2621802578d2c71b027200a135a17aebb0
Marcos Mendes <marcosdev.me@gmail.com>
2024-09-13 21:01:06 -0300

    Implements save and transcribe audio

--- talking_llm.py
+++ talking_llm.py
@@ -3,21 +3,31 @@ import os
 import numpy
 import wave
 import sounddevice
+import whisper
+import io
+import soundfile as sf
+import threading
 
 from pynput import keyboard
 from dotenv import load_dotenv, find_dotenv
+from langchain_openai import OpenAI, ChatOpenAI
+from queue import Queue
 
 load_dotenv(find_dotenv())
 
 client = openai.Client()
 
 class TalkingLLM():
-    def __init__(self):
+    def __init__(self, model="gpt-3.5-turbo-0613", whisper_size="small"):
         self.is_recording=False
         self.audio_data=[]
         self.samplerate=44100
         self.channels=1
         self.dtype='int16'
+        
+        self.whisper = whisper.load_model(whisper_size)
+        self.llm = ChatOpenAI(model=model)
+        self.llm_queue = Queue()
     
     def start_stop_recording(self):
         if self.is_recording:
@@ -41,12 +51,43 @@ class TalkingLLM():
         wav_file.setframerate(self.samplerate)
         wav_file.writeframes(numpy.array(self.audio_data, dtype=self.dtype))
         wav_file.close()
-    
+        
+        result = self.whisper.transcribe("test.wav", fp16=False)
+        response = self.llm.invoke(result["text"])
+        
+        self.llm_queue.put(response.content)
+        
     def convert_and_play(self):
-        pass
+        tts_text = ''
+        while True:
+          tts_text += self.llm_queue.get()
+          
+          if '.' in tts_text or '?' in tts_text or '!' in tts_text:
+            print(tts_text)
+            
+            spoken_response = client.audio.speech.create(
+              model='tts=1',
+              voice='fable',
+              response_format='opus',
+              input=tts_text
+            )
+            
+            buffer = io.BytesIO()
+            for chunk in spoken_response.iter_bytes(chunk_size=4096):
+              buffer.write(chunk)
+            buffer.seek(0)
+            
+            with sf.SoundFile(buffer, 'r') as sound_file:
+              data = sound_file.read(dtype='int16')
+              sounddevice.play(data, sound_file.samplerate)
+              sounddevice.wait()
+            tts_text = ''  
     
     def run(self):
         print('Running 🚀')
+        
+        thread1 = threading.Thread(target=self.convert_and_play)
+        thread1.start()
 
         def callback(indata, frame_count, time_info, status):
             if self.is_recording:
@@ -71,8 +112,7 @@ class TalkingLLM():
             with keyboard.Listener(
                 on_press=for_canonical(hotkey.press),
                 on_release=for_canonical(hotkey.release)) as l:
-                l.join()
-            
+                l.join()  
     
 if __name__ == '__main__':
     talking_llm = TalkingLLM()
